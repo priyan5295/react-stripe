2d1e3c468dfd1f67439b80227660272ceea18ce8
sathiyapriyan <sathiya.priyan@roundrockabaschool.onmicrosoft.com>
2024-06-03 17:35:04 +0530

    changes-updated

--- server.js
+++ server.js
@@ -80,3 +80,5 @@ app.post("/checkout", async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = app;
